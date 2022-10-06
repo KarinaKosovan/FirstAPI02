00726037c3a83cde50c2ae9aa646bd8ed46576b5
Karina Kosovan <karikosovan@gmail.com>
2022-10-06 10:08:08 +0200

    Adding RequestMapping

--- src/main/java/it/develhope/FirstAPI02/FactorialController.java
+++ src/main/java/it/develhope/FirstAPI02/FactorialController.java
@@ -2,14 +2,16 @@ package it.develhope.FirstAPI02;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/factorial")
 public class FactorialController {
 
     //Factorial of a non-negative integer, is multiplication of all integers smaller than or equal to n.
     // For example factorial of 6 is 6*5*4*3*2*1 which is 720.
-    @GetMapping("/factorial/{n}")
+    @GetMapping("/{n}")
     public int factorial(@PathVariable int n) {
         int factorial = 1;
         if (n < 0) {
